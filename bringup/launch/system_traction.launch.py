b81e3627f61dcbc6863d521d248174f092aba0f7
Rafael Carbonell Lázaro <racarla96@gmail.com>
2025-12-17 20:54:45 +0100

    Some changes

--- bringup/launch/system_traction.launch.py
+++ bringup/launch/system_traction.launch.py
@@ -48,24 +48,28 @@ def generate_launch_description():
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_controllers],
+        parameters=[
+            robot_description,     # ← ESTO FALTABA
+            robot_controllers
+        ],
         output="both",
     )
+
 
     
-    robot_state_pub_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
+#    robot_state_pub_node = Node(
+#        package="robot_state_publisher",
+#        executable="robot_state_publisher",
+#        output="both",
+#        parameters=[robot_description],
+#    )
 
     # Load robot state publisher
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster"],
-    )
+#    joint_state_broadcaster_spawner = Node(
+#        package="controller_manager",
+#        executable="spawner",
+#        arguments=["joint_state_broadcaster"],
+#    )
 
  
     velocity_controller_spawner = Node(
@@ -76,8 +80,8 @@ def generate_launch_description():
 
     nodes = [
         control_node,
-        robot_state_pub_node,
-        joint_state_broadcaster_spawner,
+        #robot_state_pub_node,
+        #joint_state_broadcaster_spawner,
         velocity_controller_spawner,
     ]
 
